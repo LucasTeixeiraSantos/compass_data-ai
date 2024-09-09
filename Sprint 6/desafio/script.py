9fb2e4e05f65ce3aa937844d42a9f833e0aee07b
Chob1tzu <teixeira09896@gmail.com>
2024-09-09 18:09:14 -0300

    Update

--- Sprint 6/desafio/script.py
+++ Sprint 6/desafio/script.py
@@ -19,11 +19,13 @@ def iterate_folder_and_upload_to_s3(bucket_name, data_directory):
             
 def upload_to_s3(file_path, file_name, bucket_name):
     s3_client = boto3.client('s3')
+    
     date_path = datetime.today().strftime('%Y/%m/%d')
-    file_name_upper_no_ext = os.path.splitext(file_name)[0].capitalize()
+    file_name_capitalize_no_ext = os.path.splitext(file_name)[0].capitalize()
     file_extension = os.path.splitext(file_name)[1][1:].upper()
-    s3_path = f"Raw/Local/{file_extension}/{file_name_upper_no_ext}/{date_path}/{file_name}"
+    s3_path = f"Raw/Local/{file_extension}/{file_name_capitalize_no_ext}/{date_path}/{file_name}"
     file_size = os.path.getsize(file_path)
+    
     with open(file_path, 'rb') as f:
         with tqdm(total=file_size, unit='B', unit_scale=True, desc=file_name) as pbar:
             s3_client.upload_fileobj(f, bucket_name, s3_path, Callback=lambda bytes_transferred: pbar.update(bytes_transferred))
@@ -32,10 +34,10 @@ def upload_to_s3(file_path, file_name, bucket_name):
  
 def main():
     bucket_name = "teste-bucket-luc"
-    data_directory = "C:/Users/lucas/Desktop/Projetos/compass_data-ai/Sprint 6/desafio/data"
+    data_directory = "/app/data" 
 
     verify_or_create_bucket(bucket_name)
     iterate_folder_and_upload_to_s3(bucket_name, data_directory)
-
+ 
 if __name__ == "__main__":
     main()
