1a8d912bff9c47c676bc2edc033f7c6eafeac54d
Chob1tzu <teixeira09896@gmail.com>
2024-10-07 15:31:59 -0300

    UPDATE: spark-01-dados/dados.py
    Changed to be used at google collab

--- Sprint 8/exercicios/spark-01-dados/dados.py
+++ Sprint 8/exercicios/spark-01-dados/dados.py
@@ -1,58 +1,42 @@
+# !pip install names
+
 import os
 import random
 import names
 
-# Gerar uma lista de 250 inteiros aleatórios
 lista_inteiros = [random.randint(0, 1000) for _ in range(250)]
-
-# Inverter a lista
 lista_inteiros.reverse()
+print("Lista de Inteiros:", lista_inteiros)
 
-# Imprimir o resultado
-print(lista_inteiros)
-
-# Lista de 20 animais
 lista_animais = ['Cachorro', 'Gato', 'Leão', 'Tigre', 'Elefante', 'Girafa', 'Cavalo', 'Pinguim', 'Urso', 'Raposa',
                  'Lobo', 'Canguru', 'Golfinho', 'Foca', 'Tartaruga', 'Águia', 'Leopardo', 'Baleia', 'Corvo', 'Coelho']
-
-# Ordenar a lista em ordem crescente
 lista_animais.sort()
 
-# Iterar e imprimir os animais
-[print(animal) for animal in lista_animais]
+print("Lista de Animais:")
+for animal in lista_animais:
+    print(animal)
 
-diretorio_atual = os.path.dirname(os.path.abspath(__file__))
-caminho_arquivo = os.path.join(diretorio_atual, 'animais.csv')
-# Salvar os itens em um arquivo CSV
-with open(caminho_arquivo, 'w', encoding='utf-8') as file:
+caminho_arquivo_animais = '/content/animais.csv'
+
+with open(caminho_arquivo_animais, 'w', encoding='utf-8') as file:
     for animal in lista_animais:
         file.write(animal + '\n')
 
-
-
-# Definir semente de aleatoriedade e parâmetros
 random.seed(40)
 qtd_nomes_unicos = 3000
 qtd_nomes_aleatorios = 10000000
 
-# Gerar nomes únicos
 aux = [names.get_full_name() for _ in range(qtd_nomes_unicos)]
-
 print(f"Gerando {qtd_nomes_aleatorios} nomes aleatórios")
 
-# Gerar nomes aleatórios
 dados = [random.choice(aux) for _ in range(qtd_nomes_aleatorios)]
 
-# Obter o diretório do script atual
+caminho_arquivo_nomes = '/content/nomes_aleatorios.txt'
 
-# Definir o caminho completo do arquivo
-caminho_arquivo = os.path.join(diretorio_atual, 'nomes_aleatorios.txt')
-
-# Salvar os nomes no arquivo
-with open(caminho_arquivo, 'w') as file:
+with open(caminho_arquivo_nomes, 'w', encoding='utf-8') as file:
     for nome in dados:
         file.write(nome + '\n')
 
-# Verificação: Abrir e ler o arquivo
-with open(caminho_arquivo, 'r', encoding='utf-8') as file:
-    print(file.readlines()[:10])  # Imprimir as primeiras 10 linhas para verificação
+with open(caminho_arquivo_nomes, 'r', encoding='utf-8') as file:
+    print("Primeiros 10 nomes aleatórios gerados:")
+    print(file.readlines()[:10])
