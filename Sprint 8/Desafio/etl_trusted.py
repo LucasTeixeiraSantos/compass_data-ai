450b81d8408346ffa167e2dfc68267fac7fc3495
Chob1tzu <teixeira09896@gmail.com>
2024-10-07 15:03:23 -0300

    UPDATE: desafio/lambda.py
    Changed the code to reflect the new analysis questions

---
+++ Sprint 8/Desafio/etl_trusted.py
@@ -0,0 +1,108 @@
+import sys
+import boto3
+from datetime import datetime
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import trim, col, when
+from pyspark.sql.types import StringType
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+spark = glueContext.spark_session
+job = Job(glueContext)
+
+JSON_PATH = "s3://data-lake-lucas-ts/Raw/TMDB/JSON/"
+CSV_PATH = "s3://data-lake-lucas-ts/Raw/Local/CSV/Movies"
+OUTPUT_PATH = "s3://data-lake-lucas-ts/Trusted/PARQUET/{year}/{month}/{day}/"
+
+def list_s3_files(bucket_prefix):
+    s3_client = boto3.client('s3')
+    bucket = bucket_prefix.split('/')[2]
+    prefix = '/'.join(bucket_prefix.split('/')[3:])
+    
+    result = s3_client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    files = ["s3://{}/{}".format(bucket, obj['Key']) for obj in result.get('Contents', []) if obj['Key'].endswith(('json', 'csv'))]
+    
+    return files
+
+def read_and_union_files(file_paths, file_type):
+    union_df = None
+    for file in file_paths:
+        if file_type == 'json':
+            df = spark.read.json(file)
+        elif file_type == 'csv':
+            df = spark.read.csv(file, sep='|', header=True, inferSchema=True)
+        
+        if union_df is None:
+            union_df = df
+        else:
+            union_df = union_df.union(df)
+    
+    return union_df
+
+def clean_dataframe(df):
+    for field in df.schema.fields:
+        if isinstance(field.dataType, StringType):
+            df = df.withColumn(field.name, trim(col(field.name)))
+    return df
+
+def replace_nulls(df):
+    for column in df.columns:
+        df = df.withColumn(
+            column, 
+            when((col(column) == r"\N") | (col(column) == ""), None).otherwise(col(column))
+        )
+    return df
+
+json_files = list_s3_files(JSON_PATH)
+movies_details_df = read_and_union_files(json_files, 'json')
+
+csv_files = list_s3_files(CSV_PATH)
+movies_csv_df = read_and_union_files(csv_files, 'csv')
+
+movies_details_df = clean_dataframe(movies_details_df)
+movies_csv_df = clean_dataframe(movies_csv_df)
+
+movies_csv_df = movies_csv_df.withColumnRenamed("id", "imdb_id")
+
+result_df = movies_details_df.join(movies_csv_df, on='imdb_id', how='left')
+
+result_df = result_df.select(
+    col('anoFalecimento').alias('artist_death_year'),
+    col('anoNascimento').alias('artist_birth_year'),
+    col('backdrop_path').alias('path_backdrop'),
+    col('generoArtista').alias('artist_gender'),
+    col('id').alias('id_tmdb'),
+    col('imdb_id').alias('id_imdb'),
+    col('nomeArtista').alias('artist_name'),
+    col('notaMedia').alias('vote_average_imdb'),
+    col('numeroVotos').alias('vote_count_imdb'),
+    col('original_title').alias('title_original'),
+    col('overview'),
+    col('personagem').alias('artist_character'),
+    col('poster_path').alias('path_poster'),
+    col('profissao').alias('artist_profession'),
+    col('release_date'),
+    col('revenue'),
+    col('runtime'),
+    col('tagline'),
+    col('title'),
+    col('vote_average').alias('vote_average_tmdb'),
+    col('vote_count').alias('vote_count_tmdb'),
+    col('budget')
+)
+
+result_df = replace_nulls(result_df)
+
+result_df = result_df.orderBy(col('vote_count_tmdb').desc())
+
+current_date = datetime.now()
+year, month, day = current_date.year, current_date.month, current_date.day
+parquet_output_path = OUTPUT_PATH.format(year=year, month=month, day=day)
+
+result_df.write.mode("overwrite").parquet(parquet_output_path)
+
+job.commit()
