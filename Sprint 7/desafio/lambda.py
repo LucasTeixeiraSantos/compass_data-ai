748334bdc3d2a6ec77cac38ae517054e3e54b5e7
Chob1tzu <teixeira09896@gmail.com>
2024-10-06 21:47:04 -0300

    UPDATE: desafio/lambda.py
    Changed the file considering the new scope of the challenge, now it invokes the api to get all the movies from Studio Ghibli, then invokes the api using the movie id to get more details from thes specific movie.

--- Sprint 7/desafio/lambda.py
+++ Sprint 7/desafio/lambda.py
@@ -2,38 +2,43 @@ import json
 import aiohttp
 import asyncio
 import boto3
-import os
 from botocore.exceptions import ClientError
 from datetime import datetime, timezone
 
-TMDB_API_KEY = os.environ.get('TMDB_API_KEY')  
-S3_BUCKET = os.environ.get('S3_BUCKET')
-TMDB_TYPE = os.environ.get('TMDB_TYPE')
-GENRE_ID = os.environ.get('GENRE_ID')
+TMDB_API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzI2YWIwNmM5MDBiMTMyMjU2ODE4YTIzZTU5YTBlYSIsIm5iZiI6MTcyNTAzODA5OC42MTMwODQsInN1YiI6IjY2ZDFmNmU5NmU5Nzk1M2IwNTk0M2EwMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TL6x5qI7oM3-LGsx_ih83FYerpKiFcki-iWglTxwnOs"  # Insira sua chave da API do TMDB
+S3_BUCKET = "data-lake-lucas-ts"
 
-BASE_URL = f"https://api.themoviedb.org/3/discover/{TMDB_TYPE}"
+BASE_URL_DISCOVER = "https://api.themoviedb.org/3/discover/movie"
+BASE_URL_MOVIE = "https://api.themoviedb.org/3/movie"
 HEADERS = {
     "accept": "application/json",
     "Authorization": f"Bearer {TMDB_API_KEY}"
 }
 PARAMS = {
-    "language": "en-US",
-    "sort_by": "popularity.desc",
-    "with_genres": GENRE_ID,
-    "vote_count.gte": "5"
+    "with_companies": "10342",
+    "sort_by": "popularity.desc"
 }
 
 s3 = boto3.client('s3')
 
 async def get_movies_from_api(session, page):
     params = {**PARAMS, "page": page}
-    async with session.get(BASE_URL, headers=HEADERS, params=params) as response:
+    async with session.get(BASE_URL_DISCOVER, headers=HEADERS, params=params) as response:
         if response.status == 200:
             data = await response.json()
             return data.get('results', []), data.get('total_pages', 1)
         if response.status == 429:
             await asyncio.sleep(1)
             return await get_movies_from_api(session, page)
+        response.raise_for_status()
+
+async def get_movie_details(session, movie_id):
+    async with session.get(f"{BASE_URL_MOVIE}/{movie_id}?sort_by=popularity.desc", headers=HEADERS) as response:
+        if response.status == 200:
+            return await response.json()
+        if response.status == 429:
+            await asyncio.sleep(1)
+            return await get_movie_details(session, movie_id)
         response.raise_for_status()
 
 def save_to_s3(data, file_name):
@@ -43,7 +48,7 @@ def save_to_s3(data, file_name):
         s3.put_object(
             Bucket=S3_BUCKET,
             Key=s3_key,
-            Body=json.dumps(data),
+            Body=json.dumps(data, ensure_ascii=False),  
             ContentType='application/json'
         )
         print(f"File saved at: {s3_key}")
@@ -51,39 +56,28 @@ def save_to_s3(data, file_name):
         print(f"Failed to save file: {e}")
         raise
 
-async def fetch_all_pages(session, total_pages):
-    tasks = [get_movies_from_api(session, page) for page in range(1, total_pages + 1)]
-    results = await asyncio.gather(*tasks)
-    return results
+async def fetch_all_movie_details(session, movie_ids):
+    tasks = [get_movie_details(session, movie_id) for movie_id in movie_ids]
+    return await asyncio.gather(*tasks)
 
 async def fetch_and_save_movies():
-    movie_data, file_count = [], 1
-
     async with aiohttp.ClientSession() as session:
         movies, total_pages = await get_movies_from_api(session, 1)
-
+        
+        movie_ids = [movie['id'] for movie in movies]  
+        
         if total_pages > 1:
-            results = await fetch_all_pages(session, total_pages)
+            for page in range(2, total_pages + 1):
+                movies, _ = await get_movies_from_api(session, page)
+                movie_ids.extend(movie['id'] for movie in movies)
+        
+        movie_details = await fetch_all_movie_details(session, movie_ids)
+        
+        file_name = f"movies_details.json"
+        save_to_s3(movie_details, file_name)
 
-            for movies_batch, _ in results:
-                movie_data.extend(movies_batch)
+def main():
+    asyncio.run(fetch_and_save_movies())
 
-                while len(movie_data) >= 100:
-                    file_name = f"movies_part_{str(file_count).zfill(3)}.json"
-                    save_to_s3(movie_data[:100], file_name)
-                    movie_data = movie_data[100:]
-                    file_count += 1
-
-        if movie_data:
-            file_name = f"movies_part_{str(file_count).zfill(3)}.json"
-            save_to_s3(movie_data, file_name)
-
-async def async_handler(event, context):
-    await fetch_and_save_movies()
-    return {
-        "statusCode": 200,
-        "body": json.dumps("Done")
-    }
-
-def lambda_handler(event, context):
-    return asyncio.run(async_handler(event, context))
+if __name__ == "__main__":
+    main()
